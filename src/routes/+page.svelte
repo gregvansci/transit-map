0faafcd03dd3e60d7365626729ba869c0f570f47
gregvansci <gregvansci76@hotmail.com>
2023-05-08 19:08:37 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Page in development! Please come back later.</span>
+	</Alert>
+</div>

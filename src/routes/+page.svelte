a26d786d6dd857124d51f237aa9983eac656318b
gregvansci <gregvansci76@hotmail.com>
2023-05-09 11:03:58 -0700

    Added map component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
-<script>
-	import { Alert } from 'flowbite-svelte';
+<script lang="ts">
+	// import { Alert } from 'flowbite-svelte';
+	import Map from './Map.svelte'
+	
 </script>
 
-<div class="p-8">
+<div class="w-full h-full">
+	<Map />
+</div>
+
+
+<!-- <div class="p-8">
 	<Alert>
 		<span class="font-medium">Page in development! Please come back later.</span>
 	</Alert>
-</div>
+</div> -->
---
+++ src/routes/Map.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from 'svelte';
+  import mapboxgl from 'mapbox-gl';
+  import { PUBLIC_MAPBOX_ACCESS_TOKEN } from '$env/static/public';
+
+  let map;
+	const token = PUBLIC_MAPBOX_ACCESS_TOKEN;
+  onMount(() => {
+		mapboxgl.accessToken = token;
+		map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-118.25, 33.95],
+			zoom: 9.5
+		})
+	})
+</script>
+
+<div class="" id="map"></div>
